099fd6b9d7d5fdf5719a5fc1c33b6955d7c90a74
pithydon <daniel@Daniels-iMac.local>
2020-04-23 20:22:40 -0600

    Update for Minetest 5

--- init.lua
+++ init.lua
@@ -207,57 +207,78 @@ end
 
 flowerpots.add_plant(1, "sapling", "Sapling", "default:sapling", "default_sapling.png")
 flowerpots.add_plant(1, "junglesapling", "Jungle Sapling", "default:junglesapling", "default_junglesapling.png")
+flowerpots.add_plant(1, "emergent_jungle_sapling", "Emergent Jungle Tree Sapling", "default:emergent_jungle_sapling", "default_emergent_jungle_sapling.png")
 flowerpots.add_plant(1, "pine_sapling", "Pine Sapling", "default:pine_sapling", "default_pine_sapling.png")
 flowerpots.add_plant(1, "acacia_sapling", "Acacia Tree Sapling", "default:acacia_sapling", "default_acacia_sapling.png")
+flowerpots.add_plant(1, "aspen_sapling", "Aspen Tree Sapling", "default:aspen_sapling", "default_aspen_sapling.png")
 flowerpots.add_plant(1, "dry_shrub", "Dry Shrub", "default:dry_shrub", "default_dry_shrub.png",
 		{{-0.1875, -0.5, -0.1875, 0.1875, -0.3125, 0.1875}, {-0.25, -0.3125, -0.25, 0.25, 0.125, 0.25}})
 flowerpots.add_plant(7, "cactus", "Cactus", "default:cactus", {"default_cactus_top.png", "default_cactus_side.png"})
+flowerpots.add_plant(1, "large_cactus_seedling", "Large Cactus Seedling", "default:large_cactus_seedling", "default_large_cactus_seedling.png")
+flowerpots.add_plant_with_on_place(5, "grass", "Grass", "default:grass_1", "default_grass_3.png",
+		{{-0.1875, -0.5, -0.1875, 0.1875, 0.125, 0.1875}, {-0.25, -0.3125, -0.25, 0.25, -0.125, 0.25}})
+flowerpots.add_plant_with_on_place(5, "dry_grass", "Dry Grass", "default:dry_grass_1", "default_dry_grass_3.png",
+		{{-0.1875, -0.5, -0.1875, 0.1875, 0.25, 0.1875}, {-0.25, -0.3125, -0.25, 0.25, -0.125, 0.25}})
 flowerpots.add_plant(1, "junglegrass", "Jungle Grass", "default:junglegrass", "default_junglegrass.png")
+flowerpots.add_plant_with_on_place(1, "fern", "Fern", "default:fern_1", "default_fern_1.png",
+		{{-0.1875, -0.5, -0.1875, 0.1875, -0.3125, 0.1875}, {-0.25, -0.3125, -0.25, 0.25, 0.125, 0.25}})
 flowerpots.add_plant(5, "papyrus", "Papyrus", "default:papyrus", "default_papyrus.png")
+flowerpots.add_plant(1, "bush_sapling", "Bush Sapling", "default:bush_sapling", "default_bush_sapling.png")
+flowerpots.add_plant(1, "acacia_bush_sapling", "Acacia Bush Sapling", "default:acacia_bush_sapling", "default_acacia_bush_sapling.png")
+flowerpots.add_plant(1, "pine_bush_sapling", "Pine Bush Sapling", "default:pine_bush_sapling", "default_pine_bush_sapling.png")
+flowerpots.add_plant(1, "blueberry_bush_sapling", "Blueberry Bush Sapling", "default:blueberry_bush_sapling", "default_blueberry_bush_sapling.png")
 
-if default.NAME == "tng" then
-	flowerpots.add_plant(1, "birch_sapling", "Birch Tree Sapling", "default:birch_sapling", "default_birch_sapling.png")
-	flowerpots.add_plant(1, "maple_sapling", "Maple Tree Sapling", "default:maple_sapling", "default_maple_sapling.png")
-	flowerpots.add_plant(1, "red_maple_sapling", "Red Maple Tree Sapling", "default:red_maple_sapling", "default_red_maple_sapling.png")
-	flowerpots.add_plant(1, "cherry_sapling", "Cherry Tree Sapling", "default:cherry_sapling", "default_cherry_sapling.png")
-	flowerpots.add_plant_with_on_place(5, "grass", "Grass", "default:grass_5", "default_grass_3.png",
-			{{-0.1875, -0.5, -0.1875, 0.1875, 0.125, 0.1875}, {-0.25, -0.3125, -0.25, 0.25, -0.125, 0.25}})
-	flowerpots.add_plant_with_on_place(5, "dry_grass", "Dry Grass", "default:dry_grass_5", "default_dry_grass_3.png",
-			{{-0.1875, -0.5, -0.1875, 0.1875, 0.25, 0.1875}, {-0.25, -0.3125, -0.25, 0.25, -0.125, 0.25}})
-else
-	flowerpots.add_plant(1, "aspen_sapling", "Aspen Tree Sapling", "default:aspen_sapling", "default_aspen_sapling.png")
-	flowerpots.add_plant_with_on_place(5, "grass", "Grass", "default:grass_1", "default_grass_3.png",
-			{{-0.1875, -0.5, -0.1875, 0.1875, 0.125, 0.1875}, {-0.25, -0.3125, -0.25, 0.25, -0.125, 0.25}})
-	flowerpots.add_plant_with_on_place(5, "dry_grass", "Dry Grass", "default:dry_grass_1", "default_dry_grass_3.png",
-			{{-0.1875, -0.5, -0.1875, 0.1875, 0.25, 0.1875}, {-0.25, -0.3125, -0.25, 0.25, -0.125, 0.25}})
-end
+minetest.register_node("flowerpots:pot_marram_grass", {
+	description = "Marram Grass in a pot.",
+	tiles = {{name = "flowerpots_pot.png", backface_culling = true}, {name = "default_sand.png", backface_culling = true}, "default_marram_grass_3.png"},
+	paramtype = "light",
+	paramtype2 = "facedir",
+	drawtype = "mesh",
+	mesh = "flowerpots_pot_1.obj",
+	collision_box = {
+		type = "fixed",
+		fixed = {
+			{-0.1875, -0.5, -0.1875, 0.1875, -0.1875, 0.1875},
+			{-0.1875, -0.3125, 0.1875, 0.25, -0.125, 0.25},
+			{-0.25, -0.3125, -0.1875, -0.1875, -0.125, 0.25},
+			{-0.25, -0.3125, -0.25, 0.1875, -0.125, -0.1875},
+			{0.1875, -0.3125, -0.25, 0.25, -0.125, 0.1875}
+		}
+	},
+	selection_box = {
+		type = "fixed",
+		fixed = {
+			{-0.1875, -0.5, -0.1875, 0.1875, -0.3125, 0.1875},
+			{-0.25, -0.3125, -0.25, 0.25, 0.5, 0.25}
+		}
+	},
+	groups = {snappy = 3, cracky = 3, oddly_breakable_by_hand = 3, not_in_creative_inventory = 1},
+	on_punch = flowerpots.take_plant("default:marram_grass_1")
+})
+flowerpots.override_on_place("default:marram_grass_1")
+flowerpots.plants["default:marram_grass_1"] = "flowerpots:pot_marram_grass"
 
 if minetest.get_modpath("farming") then
-	if default.NAME == "tng" then
-		flowerpots.add_plant(1, "garden", "Garden", "farming:garden", "farming_garden.png")
-		flowerpots.add_plant_with_on_place(5, "wheat", "Wheat", "farming:wheat_seed", "farming_wheat_8.png")
-		flowerpots.add_plant_with_on_place(1, "cotton", "Cotton", "farming:cotton_seed", "farming_cotton_8.png")
-		flowerpots.add_plant_with_on_place(1, "potato", "Potato", "farming:potato", "farming_potato_3.png")
-		flowerpots.add_plant_with_on_place(1, "strawberry", "Strawberries", "farming:strawberry", "farming_strawberry_7.png")
-		flowerpots.add_plant_with_on_place(1, "lettuce", "Lettuce", "farming:lettuce", "farming_lettuce_7.png")
-	else
-		flowerpots.add_plant_with_on_place(5, "wheat", "Wheat", "farming:seed_wheat", "farming_wheat_8.png")
-		flowerpots.add_plant_with_on_place(1, "cotton", "Cotton", "farming:seed_cotton", "farming_cotton_8.png")
-	end
+	flowerpots.add_plant_with_on_place(5, "wheat", "Wheat", "farming:seed_wheat", "farming_wheat_8.png")
+	flowerpots.add_plant_with_on_place(1, "cotton", "Cotton", "farming:seed_cotton", "farming_cotton_8.png")
 end
 
 if minetest.get_modpath("flowers") then
-	flowerpots.add_plant(1, "rose", "Rose", "flowers:rose", "flowers_rose.png")
-	flowerpots.add_plant(1, "tulip", "Tulip", "flowers:tulip", "flowers_tulip.png")
-	flowerpots.add_plant(1, "dandelion_yellow", "Yellow Dandelion", "flowers:dandelion_yellow", "flowers_dandelion_yellow.png")
+	flowerpots.add_plant(1, "rose", "Red Rose", "flowers:rose", "flowers_rose.png")
+	flowerpots.add_plant(1, "tulip", "Orange Tulip", "flowers:tulip", "flowers_tulip.png")
+	flowerpots.add_plant(1, "dandelion_yellow", "Yellow Dandelion", "flowers:dandelion_yellow", "flowers_dandelion_yellow.png",
+			{{-0.1875, -0.5, -0.1875, 0.1875, -0.3125, 0.1875}, {-0.25, -0.3125, -0.25, 0.25, 0.1125, 0.25}})
+	flowerpots.add_plant(1, "chrysanthemum_green", "Green Chrysanthemum", "flowers:chrysanthemum_green", "flowers_chrysanthemum_green.png",
+			{{-0.1875, -0.5, -0.1875, 0.1875, -0.3125, 0.1875}, {-0.25, -0.3125, -0.25, 0.25, 0.1125, 0.25}})
 	flowerpots.add_plant(1, "geranium", "Blue Geranium", "flowers:geranium", "flowers_geranium.png")
 	flowerpots.add_plant(1, "viola", "Viola", "flowers:viola", "flowers_viola.png",
 			{{-0.1875, -0.5, -0.1875, 0.1875, -0.3125, 0.1875}, {-0.25, -0.3125, -0.25, 0.25, 0.1125, 0.25}})
 	flowerpots.add_plant(1, "dandelion_white", "White Dandelion", "flowers:dandelion_white", "flowers_dandelion_white.png",
 			{{-0.1875, -0.5, -0.1875, 0.1875, -0.3125, 0.1875}, {-0.25, -0.3125, -0.25, 0.25, 0.1125, 0.25}})
+	flowerpots.add_plant(1, "tulip_black", "Black Tulip", "flowers:tulip_black", "flowers_tulip_black.png")
 	flowerpots.add_plant(1, "mushroom_red", "Red Mushroom", "flowers:mushroom_red", "flowers_mushroom_red.png",
 			{{-0.1875, -0.5, -0.1875, 0.1875, -0.3125, 0.1875}, {-0.25, -0.3125, -0.25, 0.25, 0.3125, 0.25}})
 	flowerpots.add_plant(1, "mushroom_brown", "Brown Mushroom", "flowers:mushroom_brown", "flowers_mushroom_brown.png",
 			{{-0.1875, -0.5, -0.1875, 0.1875, -0.3125, 0.1875}, {-0.25, -0.3125, -0.25, 0.25, 0.3125, 0.25}})
-	flowerpots.add_plant_with_on_place(6, "waterlily", "Waterlily", "flowers:waterlily", {"flowers_waterlily.png", "flowers_waterlily.png^[transformFY"})
+	flowerpots.add_plant_with_on_place(6, "waterlily", "Waterlily", "flowers:waterlily", {"flowers_waterlily.png", "flowers_waterlily_bottom.png"})
 end
